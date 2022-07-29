678f291e417e40600c5fe059aa03b45407835d44
Loup Aubour <laubour@arneogroup.com>
2022-07-29 10:11:29 +0200

    BILLED-03: Bug Report for Bills, sorting data feature added

--- bill-app/Billed-app-FR-Front/src/views/BillsUI.js
+++ bill-app/Billed-app-FR-Front/src/views/BillsUI.js
@@ -20,7 +20,10 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  const sortedData =
+    data && data.length ? data.sort((a, b) => new Date(b.date) - new Date(a.date)) : "";
+
+    return data && data.length ? sortedData.map((bill) => row(bill)).join("") : "";
 }
 
 export default ({ data: bills, loading, error }) => {
